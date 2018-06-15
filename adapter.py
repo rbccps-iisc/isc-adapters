05cc076a7022be3d0afca9d03c7df05070572985
Shubham Pharande <shubhampharande8@gmail.com>
2018-06-15 19:49:32 +0530

    Added celery implementation

--- adapter.py
+++ adapter.py
@@ -1,72 +1,125 @@
 from MQTTPubSub import MQTTPubSub
 from AMQPPubSub import AMQPPubSub
+from time import sleep
+import redis, hiredis
+import base64 as bs
+from celery import Celery
+import zmq
+import json
+import importlib.machinery
+import os
+import sys
+from multiprocessing import Process
 from google.protobuf import json_format
 from google.protobuf.json_format import MessageToDict
-import time
-import sys
-import json
-import base64
-import threading
+from jsonschema import validate
+import ast
 
-import zmq
-import os
-import importlib.machinery
+#---------------------------------------------------------------------------------------------------------------------
 
+#Code for celery tasks to be implemented when messages arrive to the MQTT topic(s)
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+appd=Celery('print', broker='redis://localhost/0')
+
+@appd.task
+def decode_push():
+    global r
+    inc_dict=ast.literal_eval(r.lpop("MQ-side-messages").decode('utf-8'))
+    payload=(inc_dict.keys()[0]).decode('utf-8')
+    device_id=inc_dict.values()[0]
+    #code for proto decode
+    try:
+        if device_id in modules:
+            ns_sensor_message = modules[device_id]["protoFrom"]
+            jsonData = json.loads(payload)
+            decodedData = base64.b64decode(jsonData["data"])
+            ns_sensor_message.ParseFromString(decodedData)
+            mw_message = MessageToDict(ns_sensor_message)
+            print (mw_message)
+            mwSub.publish(itemId,json.dumps(mw_message))
+    except Exception as e:
+        print("DECODE ERROR")
+        print(e)
+
+#------------------------------------------------------------------------------------------------------------------------
+
+#Defining various callbacks for MQTT and AMQP connections
+
+def MWSub_onMessage(ch, method, properties, body):
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! IS THIS POSSIBLE? !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    #Change according to wildcard entry 
+    try:
+        _id = method.routing_key.replace('_update','')
+        print(_id)
+        mw_actuation_message = modules[_id]["protoTo"]
+        print('Received ', _id, ' from MW')
+        data = {}
+        data['reference'] = 'a'
+        data['confirmed'] = False
+        data['fport'] = 1
+        print(body)
+        json_format.Parse(body, mw_actuation_message, ignore_unknown_fields=False)
+        data['data'] = (base64.b64encode(mw_actuation_message.SerializeToString())).decode("utf-8")
+#Proto encode and publish to appropriate topic
+        schema(json=data['data'], devId= _id)
+        if(validationFlag):
+            nsSub.publish(ns_tx_topic.replace("{id}", _id), json.dumps(data))
+
+    except Exception as e:
+        print("ENCODE ERROR")
+        print(e)
+
+
+
+def NSSub_onConnect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+
+
+def NSSub_onMessage(client, userdata, msg):
+    global r
+    mq-dic={}
+    mq-dic={msg.payload.decode('utf-8'):msg.topic.split("/")[3]}
+    r.rpush("MQ-side-messages", mq-dic)
+    decode_push.delay()
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+#To import proto files to the code dynamically
 
 adaptersDir = os.getcwd() + "/adapters"
 cwd = os.getcwd()
 
+modules = {}
 items = {}
+
 
 ns_rx_topic = "application/1/node/{id}/rx"
 ns_tx_topic = "application/1/node/{id}/tx"
 
-
-modules = {}
-
-
-def loadModule(itemEntry):
-
-    global modules
-
-    itemId = itemEntry["id"]
-    modules[itemId] = {}
-    try:
-        from_spec = importlib.util.spec_from_file_location('from_' + itemId + '_pb2',
-                                                           adaptersDir + '/id_' + itemId + '/from_' + itemId + '_pb2.py')
-        from_mod = importlib.util.module_from_spec(from_spec)
-        from_spec.loader.exec_module(from_mod)
-        modules[itemId]["protoFrom"] = getattr(from_mod, itemEntry["protoFrom"])()
-    except Exception as e:
-        print(e)
-
-    try:
-        to_spec = importlib.util.spec_from_file_location('to_' + itemId + '_pb2',
-                                                         adaptersDir + '/id_' + itemId + '/to_' + itemId + '_pb2.py')
-        to_mod = importlib.util.module_from_spec(to_spec)
-        to_spec.loader.exec_module(to_mod)
-        modules[itemId]["protoTo"] = getattr(to_mod, itemEntry["protoTo"])()
-    except Exception as e:
-        print(e)
-
-
-def getModule(itemId):
-
-    global modules
-
-    try:
-        return modules[itemId]
-    except:
-        return None
-
-
+validationFlag = False
 
 try:
     with open(cwd + '/items.json', 'r') as f:
         items = json.load(f)
-        for item in items:
+        for item in items.keys(): 
             try:
-                loadModule(items[item])
+                modules[item] = {}
+                from_spec = importlib.util.spec_from_file_location('from_' + item + '_pb2', adaptersDir + '/id_' + item + '/from_' + item + '_pb2.py')
+                from_mod = importlib.util.module_from_spec(from_spec)
+                from_spec.loader.exec_module(from_mod)
+                modules[item]["protoFrom"] = getattr(from_mod,items[item]["protoFrom"])()
+
+                to_spec = importlib.util.spec_from_file_location('to_' + item + '_pb2', adaptersDir + '/id_' + item + '/to_' + item + '_pb2.py')
+                to_mod = importlib.util.module_from_spec(to_spec)
+                to_spec.loader.exec_module(to_mod)
+                modules[item]["protoTo"] = getattr(to_mod, items[item]["protoTo"])()
             except Exception as e:
                 print("Couldn't load", item)
                 print(e)
@@ -74,96 +127,62 @@ except:
     print("Couldn't load")
 
 
+
+def schema(json, devId):
+
+    print('in function schema.....')
+    with urllib.request.urlopen('https://smartcity.rbccps.org/api/0.1.0/cat') as resp:
+        data = json.loads(resp.read().decode())
+        sensor_data = data['items']
+        data.clear()
+
+    sensor_schema = {}
+    for i in range(0, len(sensor_data)):
+        sensor_schema[sensor_data[i]['id']] = sensor_data[i]['data_schema']
+
+    try :
+        validate(instance= json, schema= sensor_schema[devId])
+        validationFlag = True
+
+    except Exception as e:
+        print('given device data is not valid to its schema..')
+        validationFlag = False
+
+
+
 def server():
-
-    global modules
-
+    
     context = zmq.Context()
     socket = context.socket(zmq.SUB)
     socket.setsockopt_string(zmq.SUBSCRIBE, '')
     socket.bind("tcp://*:%s" % 5555)
     while True:
         message = socket.recv()
-        print("Received request  %s" % message)
-        itemEntry = json.loads(str(message, 'utf-8'))
+        print("Received request  %s" %  message)
+        itemEntry = json.loads(str(message,'utf-8'))
+        itemId = itemEntry["id"]
+        modules[itemId] = {}
+	#Import from files for decoding and to files for encoding when new device is regsitered
         try:
-            loadModule(itemEntry)
-        except Exception as e:
-            print ("Couldn't Load Module", e)
+            from_spec = importlib.util.spec_from_file_location('from_' + itemId + '_pb2', adaptersDir + '/id_' + itemId + '/from_' + itemId + '_pb2.py')
+            from_mod = importlib.util.module_from_spec(from_spec)
+            from_spec.loader.exec_module(from_mod)
+            modules[itemId]["protoFrom"] = getattr(from_mod, itemEntry["protoFrom"])()
+            to_spec = importlib.util.spec_from_file_location('to_' + itemId + '_pb2', adaptersDir + '/id_' + itemId + '/to_' + itemId + '_pb2.py')
+            to_mod = importlib.util.module_from_spec(to_spec)
+            to_spec.loader.exec_module(to_mod)
+            modules[itemId]["protoTo"] = getattr(to_mod, itemEntry["protoTo"])()
+        except:
+            print("Couldn't load objects")
+            print(sys.exc_traceback.tb_lineno)
 
-
-
-
-serverThread = threading.Thread(target=server)
-serverThread.start()
+Process(target=server).start()
 
 protosJson = {}
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-def NSSub_onMessage(mqttc, obj, msg):
-
-
-    global modules
-
-# NS Message topics are of the form application/{applicationId}/node/{id}/rx
-    try:
-        try:
-            topic = msg.topic.split('/')
-            itemId = topic[3] #{id} is the 4th field
-            print('Received ', itemId, ' from NS')
-        except Exception as e:
-            print("ignored", topic)
-
-
-        ns_sensor_message = modules[itemId]["protoFrom"]
-        jsonData = json.loads((msg.payload).decode("utf-8"))
-        decodedData = base64.b64decode(jsonData["data"])
-        ns_sensor_message.ParseFromString(decodedData)
-        mw_message = MessageToDict(ns_sensor_message)
-        print (mw_message)
-        mwSub.publish(itemId,json.dumps(mw_message))
-    except Exception as e:
-        print("DECODE ERROR")
-        print(e)
-
-
-
-
-
-def MWSub_onMessage(ch, method, properties, body):
-
-
-#Change according to wildcard entry
-
-    if("_update" in method.routing_key  ):
-        _id = method.routing_key.replace('_update','')
-        try:
-            mw_actuation_message = modules[_id]["protoTo"]
-            print('Received ', _id, ' from MW')
-            data = {}
-            data['reference'] = 'a'
-            data['confirmed'] = False
-            data['fport'] = 1
-            json_format.Parse(body, mw_actuation_message, ignore_unknown_fields=False)
-            print(body)
-            data['data'] = (base64.b64encode(mw_actuation_message.SerializeToString())).decode("utf-8")
-            nsSub.publish(ns_tx_topic.replace("{id}", _id), json.dumps(data))
-            mw_actuation_message.Clear()
-        except Exception as e:
-            print(_id, " Failed Decoding", e)
-    else:
-        print("Ignored", method.routing_key)
-
-
-
-def NSSub_onConnect(client, userdata, flags, rc):
-    print("Connected to NS SUB result code " + str(rc))
-
-
-def NSPub_onConnect(client, userdata, flags, rc):
-    print("Connected to NS PUB with result code " + str(rc))
-
+#Connection parameters for AMQP and MQTT connections
 
 mwSubParams = {}
 mwSubParams["url"] = "10.156.14.6"
@@ -174,6 +193,8 @@ mwSubParams["username"] = "admin"
 mwSubParams["password"] = "admin@123"
 mwSubParams["exchange"] = "amq.topic"
 mwSub = AMQPPubSub(mwSubParams)
+
+
 
 nsSubParams = {}
 nsSubParams["url"] = "gateways.rbccps.org"
@@ -188,13 +209,19 @@ nsSub = MQTTPubSub(nsSubParams)
 
 
 
+#----------------------------------------------------------------------------
+
+
+
 def main():
+
     mwSub_rc = mwSub.run()
     nsSub_rc = nsSub.run()
 
+
+
     while True:
-        time.sleep(10)
+        sleep(10)
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
--- api.py
+++ api.py
@@ -1,15 +1,12 @@
-#!/usr/bin/python3
-
-from flask import Flask, request 
+from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
-import json 
+import json
 import requests
 import os
 import subprocess as sub
 import sys
 from shutil import copyfile
 import zmq
-
 
 app = Flask(__name__)
 api = Api(app)
@@ -38,13 +35,11 @@ socket.connect ("tcp://localhost:%s" % 5555)
 
 
 
-
 catURL = ""
 protoURL = ""
 
 class Register(Resource):
     def post(self):
-        itemEntry = {}
         flag = 0
         try:
             json_data = request.get_json()
@@ -53,40 +48,36 @@ class Register(Resource):
             id = catJSON["items"][0]["id"]
             adapterRoot = workingDir + '/adapters/id_' + id
             os.mkdir(adapterRoot, mode = 0o755)
-            
+
             try:
                 protoTo = catJSON["items"][0]["serialization_to_device"]["schema_ref"]
-                if protoTo is not None:
-                    protoToLink = protoTo["link"]
-                    with open(adapterRoot + '/to_' + id + '.proto','wb') as file:
-                        resp = requests.get(protoToLink)
-                        file.write(resp.content)
-                    p = sub.call('protoc -I=' + adapterRoot + ' --python_out=' + adapterRoot + ' ' +  adapterRoot + '/to_' + id + '.proto'  ,shell=True)
-                    itemEntry["protoTo"] = catJSON["items"][0]["serialization_to_device"]["schema_ref"]["mainMessageName"]
-                flag = flag + 1 
-            except:
+                protoToLink = protoTo["link"]
+                with open(adapterRoot + '/to_' + id + '.proto','wb') as file:
+                    resp = requests.get(protoToLink)
+                    file.write(resp.content)
+                p = sub.call('protoc -I=' + adapterRoot + ' --python_out=' + adapterRoot + ' ' +  adapterRoot + '/to_' + id + '.proto'  ,shell=True)
+                itemEntry["protoTo"] = catJSON["items"][0]["serialization_to_device"]["schema_ref"]["mainMessageName"]
                 flag = flag + 1
+            except:
                 print("Couldn't get *To* Proto")
 
 
             try:
                 protoFrom = catJSON["items"][0]["serialization_from_device"]["schema_ref"]
-                if protoFrom is not None:
-                    protoFromLink = protoFrom["link"]
-                    with open(adapterRoot + '/from_' + id + '.proto','wb') as file:
-                        resp = requests.get(protoFromLink)
-                        file.write(resp.content)
+                protoFromLink = protoFrom["link"]
+                with open(adapterRoot + '/from_' + id + '.proto','wb') as file:
+                    resp = requests.get(protoFromLink)
+                    file.write(resp.content)
 
-                    p = sub.call('protoc -I=' + adapterRoot + ' --python_out=' + adapterRoot + ' ' +  adapterRoot + '/from_' + id + '.proto'  ,shell=True)
-                    itemEntry["protoFrom"] = catJSON["items"][0]["serialization_from_device"]["schema_ref"]["mainMessageName"]
+                p = sub.call('protoc -I=' + adapterRoot + ' --python_out=' + adapterRoot + ' ' +  adapterRoot + '/from_' + id + '.proto'  ,shell=True)
+                itemEntry["protoFrom"] = catJSON["items"][0]["serialization_from_device"]["schema_ref"]["mainMessageName"]
                 flag = flag + 1
             except:
-                flag = flag + 1
                 print("Couldn't get *From* Proto")
 
-            itemEntry["id"] = id
-            items[id]=itemEntry
 
+            items[id]=itemEntry
+            itemEntry["id"] = id
             print(itemEntry)
             with open(workingDir + '/items.json', 'w') as jsFile:
                 json.dump(items, jsFile)
@@ -98,10 +89,10 @@ class Register(Resource):
         except Exception as e:
             print(e)
 
-            
-
-	
 api.add_resource(Register, '/register')
 
-if __name__ == '__main__':
-    app.run(debug=True,host="10.156.14.16")
+def main():
+   app.run(debug=True)
+
+if __name__=="__main__":
+   main()
