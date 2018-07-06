a7c249e287398b0ca0cbe7824c6ac1a864cf3928
Shubham Pharande <shubhampharande8@gmail.com>
2018-07-06 10:14:37 +0530

    Server registration v0.4

--- adapter.py
+++ adapter.py
@@ -23,8 +23,6 @@ redConn = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 scheduler = AsyncIOScheduler()
 
-poll_url = "" < SPECIFY MASTER URL FOR POLLING HERE >
-
 #----------------------------------------------------------------------------------------------------#
 
 # MongoDB setup
@@ -55,9 +53,8 @@ def decode_push():
     if dec_device_id in http_items:
         b64en = jsonData["data"]
         decodedData = base64.b64decode(b64en.encode('utf-8'))
-        mwSub.publish(dec_device_id+"_tx",
-                      json.dumps(decodedData.decode('utf-8')))
-    elif dec_device_id in modules:
+        mwSub.publish(dec_device_id+'_tx', json.dumps(decodedData.decode('utf-8')))	    ## tx for device transmission
+    elif dec_device_id in items:
         # code for proto decode
         try:
             b64en = jsonData["data"]
@@ -87,7 +84,7 @@ def encode_push():
         print(en_body)
         json_format.Parse(en_body, mw_actuation_message, ignore_unknown_fields=False)
         data["data"] = base64.b64encode(mw_actuation_message.SerializeToString()).decode('utf-8')
-        nsSub.publish(ns_rx_topic.replace("{id}", _id), json.dumps(data))
+        nsSub.publish(items[en_id]["getAdd"], json.dumps(data))
     except Exception as e:
         print("ENCODE ERROR")
         print(e)
@@ -95,7 +92,7 @@ def encode_push():
 
 @celery_app.task
 def poll_to_url(device_id):
-    r = requests.get("http://"+poll_url+"/"+device_id)
+    r = requests.get(http_items[device_id]["getAdd"])
     if r.json is not None:
         data = {}
         data["reference"] = "a"
@@ -114,7 +111,7 @@ def poll_to_url(device_id):
 
 
 def MWSub_onMessage(ch, method, properties, body):
-    if '_rx' in method.routing_key:
+    if '_rx' in method.routing_key:						## rx for device reception
         # Change according to wildcard entry
         d_id = method.routing_key.replace('_update', '')
         _id = d_id[:len(d_id)-3]
@@ -123,6 +120,8 @@ def MWSub_onMessage(ch, method, properties, body):
         redConn.rpush("outgoing-messages", am_dict)
         print("Received", am_dict)
         encode_push.delay()
+    else:
+	pass
 #_____!!!!!!ADD A CHECK AND METHOD FOR HTTP DEVICES UP THERE, IN CASE IT IS IMPLEMENTED IN FUTURE!!!!!!_____#
 
 
@@ -131,7 +130,6 @@ def NSSub_onConnect(client, userdata, flags, rc):
 
 
 def NSSub_onMessage(client, userdata, msg):
-    global r
     in_str = json.dumps(json.loads(msg.payload.decode('utf-8')))
     mq_dict = {}
     mq_dict = {msg.topic.split('/')[3]: in_str}
@@ -148,11 +146,9 @@ adaptersDir = os.getcwd() + "/adapters"
 cwd = os.getcwd()
 
 modules = {}
-http_items = []
-items = {}
 
-ns_rx_topic = "application/1/node/{id}/rx"  # Messages from middleware
-ns_tx_topic = "application/1/node/{id}/tx"  # Messages to middleware
+http_items = {}
+items = {}
 
 try:
     mres = mcln.find(projection={'_id': False})
@@ -161,7 +157,7 @@ try:
 
     hres = hcln.find(projection={'_id': False})
     for ids in hres:
-        http_items.append(ids["id"])
+        http_items.update(ids)
 
     for item in list(items.keys()):
         try:
@@ -169,22 +165,13 @@ try:
             from_spec = importlib.util.spec_from_file_location('from_' + item + '_pb2', adaptersDir + '/id_' + item + '/from_' + item + '_pb2.py')
             from_mod = importlib.util.module_from_spec(from_spec)
             from_spec.loader.exec_module(from_mod)
-            modules[item]["protoFrom"] = getattr(
-                from_mod, items[item]["protoFrom"])()
+            modules[item]["protoFrom"] = getattr(from_mod, items[item]["protoFrom"])()
 
             to_spec = importlib.util.spec_from_file_location('to_' + item + '_pb2', adaptersDir + '/id_' + item + '/to_' + item + '_pb2.py')
             to_mod = importlib.util.module_from_spec(to_spec)
             to_spec.loader.exec_module(to_mod)
-            modules[item]["protoTo"] = getattr(
-                to_mod, items[item]["protoTo"])()
+            modules[item]["protoTo"] = getattr(to_mod, items[item]["protoTo"])()
 
-            # CONFIRM CODE HERE WITH FILE NAMES AND FILE TYPES
-            api_spec = importlib.utli.spec_from_file_location('server_' + item, adaptersDir + '/id_' + item + '/server' + item + )  # COMPLETE THE ADDRESS HERE
-            api_mod = importlib.util.module_from_spec(api_spec)
-            api_spec.loader.exec_module(api_mod)
-            modules[item]["serverName"] = getattr(api_mod, items[item]["serverName"])()
-            modules[item]["getAddress"] = getattr(api_mod, items[item]["get"])()
-            modules[item]["postAddress"] = getattr(api_mod, items[item]["post"])()
         except Exception as e:
             print("Couldn't load", item)
             print(e)
@@ -193,21 +180,14 @@ except Exception as e:
 
 
 try:
-    for devID in http_items:
+    for ids in http_items:
+	devID = ids["id"]
         try:
             scheduler.add_job(func=poll_to_url.delay, args=[devID], trigger='interval', seconds=10)
             print("Added job for ID", devID)
         except Exception as e:
             print("Couldn't add process with ID", devID)
             print(e)
-
-# CORRECT FILE NAME AND TYPE HERE
-        api_spec = importlib.utli.spec_from_file_location('server_' + item, adaptersDir + '/id_' + item + '/server' + item + )  # COMPLETE THE ADDRESS HERE
-        api_mod = importlib.util.module_from_spec(api_spec)
-        api_spec.loader.exec_module(api_mod)
-        modules[item]["serverName"] = getattr(api_mod, items[item]["serverName"])()
-        modules[item]["getAddress"] = getattr(api_mod, items[item]["get"])()
-        modules[item]["postAddress"] = getattr(api_mod, items[item]["post"])()
 
 except Exception as e:
     print("Couldn't add processes")
@@ -233,12 +213,14 @@ def server():
         print("Received request  %s" % message)
         itemEntry = json.loads(str(message, 'utf-8'))
         itemId = itemEntry["id"]
-        if len(list(itemEntry.keys())) == 1:
-            http_items.append(itemId)
+        if len(list(itemEntry.keys())) == 8:
+            modules[itemId]={}
+	    http_items.update(itemEntry)
             try:
                 scheduler.add_job(pool_to_url(itemId), 'interval', seconds=10)
             except:
                 print("Couldn't add job for ID", itemId, "after registration")
+	    
         else:
             modules[itemId] = {}
             try:
@@ -253,20 +235,11 @@ def server():
                 to_spec.loader.exec_module(to_mod)
                 modules[itemId]["protoTo"] = getattr(to_mod, itemEntry["protoTo"])()
 
-                # CORRECT FILE NAME AND TYPE
-                api_spec = importlib.utli.spec_from_file_location('server_' + item, adaptersDir + '/id_' + item + '/server' + item + )  # COMPLETE THE ADDRESS HERE
-                api_mod = importlib.util.module_from_spec(api_spec)
-                api_spec.loader.exec_module(api_mod)
-                modules[item]["serverName"] = getattr(api_mod, items[item]["serverName"])()
-                modules[item]["getAddress"] = getattr(api_mod, items[item]["get"])()
-                modules[item]["postAddress"] = getattr(api_mod, items[item]["post"])()
             except:
                 print("Couldn't load objects")
 
 
 Process(target=server).start()
-
-protosJson = {}
 
 #----------------------------------------------------------------------------------------------------#
 
@@ -287,7 +260,7 @@ nsSubParams = {}
 nsSubParams["url"] = "gateways.rbccps.org"
 nsSubParams["port"] = 1883
 nsSubParams["timeout"] = 60
-nsSubParams["topic"] = "application/1/node/+/rx"
+nsSubParams["topic"] = "application/1/node/+/tx"
 nsSubParams["onMessage"] = NSSub_onMessage
 nsSubParams["onConnect"] = NSSub_onConnect
 nsSubParams["username"] = "loraserver"
--- api.py
+++ api.py
@@ -1,5 +1,3 @@
-# --------!!!!!!!!!!!! ONGOING: Addding server registration !!!!!!!!!!!-------
-
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 import json
@@ -14,24 +12,45 @@ import pymongo
 app = Flask(__name__)
 api = Api(app)
 
+
 client = pymongo.MongoClient()
 mdb = client.devices_db
 mcln = mdb.devices
 hdb = client.devices_db_http
 hcln = hdb.devices
+sdb = client.servers_db
+scln = sdb.servers
 
 
 workingDir = sys.path[0]
 items = {}
+servers = {}
+
 
 try:
     res = mcln.find(projections={'_id': False})
     for ids in res:
         items.update(ids)
 except:
-    print("Couldn't load list")
+    print("Couldn't load MQTT list")
+
+try:
+    res = hcln.find(projections={'_id': False})
+    for ids in res:
+        items.update(ids)
+except:
+    print("Couldn't load HTTP list")
+
+try:
+    res = scln.find(projections={'_id': False})
+    for ids in res:
+        servers.update(ids)
+except:
+    print("Couldn't load servers list")
+
 
 itemEntry = {}
+
 
 context = zmq.Context()
 print("Connecting to Adapter with ports %s" % 1617)
@@ -39,7 +58,7 @@ socket = context.socket(zmq.PUB)
 socket.connect("tcp://localhost:%s" % 1617)
 
 #
-#   { "70b3d58ff01201" : { "protoTo" : "msgName", "protoFrom":"msgName", "serverName" : "name", "get" : "link", "post" : "link" }}
+#   { "70b3d58ff01201" : { "protoTo" : "msgName", "protoFrom":"msgName", "serverName" : "name", "getAdd" : "link", "postAdd" : "link" }}
 #
 
 
@@ -47,7 +66,7 @@ catURL = ""
 protoURL = ""
 
 
-class Register(Resource):
+class DeviceRegister(Resource):
     def post(self):
         flag = 0
         try:
@@ -58,39 +77,89 @@ class Register(Resource):
             adapterRoot = workingDir + '/adapters/id_' + id
             os.mkdir(adapterRoot, mode=0o755)
 
-            try:
-                protoTo = catJSON["items"][0]["serialization_to_device"]["schema_ref"]
-                protoToLink = protoTo["link"]
-                with open(adapterRoot + '/to_' + id + '.proto', 'wb') as file:
-                    resp = requests.get(protoToLink)
-                    file.write(resp.content)
-                p = sub.call('protoc -I=' + adapterRoot + ' --python_out=' + adapterRoot + ' ' + adapterRoot + '/to_' + id + '.proto', shell=True)
-                itemEntry["protoTo"] = catJSON["items"][0]["serialization_to_device"]["schema_ref"]["mainMessageName"]
-                flag = flag + 1
-            except:
-                print("Couldn't get *To* Proto")
+            if(catJSON["items"][0]["server_config"]["protocol"] == ("mqtt" or "MQTT)")):
+                try:
+                    protoTo = catJSON["items"][0]["serialization_to_device"]["schema_ref"]
+                    protoToLink = protoTo["link"]
+                    with open(adapterRoot + '/to_' + id + '.proto', 'wb') as file:
+                        resp = requests.get(protoToLink)
+                        file.write(resp.content)
+                    p = sub.call('protoc -I=' + adapterRoot + ' --python_out=' +
+                                 adapterRoot + ' ' + adapterRoot + '/to_' + id + '.proto', shell=True)
+                    itemEntry["protoTo"] = catJSON["items"][0]["serialization_to_device"]["schema_ref"]["mainMessageName"]
+                    flag = flag + 1
+                except:
+                    print("Couldn't get *To* Proto")
 
-            try:
-                protoFrom = catJSON["items"][0]["serialization_from_device"]["schema_ref"]
-                protoFromLink = protoFrom["link"]
-                with open(adapterRoot + '/from_' + id + '.proto', 'wb') as file:
-                    resp = requests.get(protoFromLink)
-                    file.write(resp.content)
+                try:
+                    protoFrom = catJSON["items"][0]["serialization_from_device"]["schema_ref"]
+                    protoFromLink = protoFrom["link"]
+                    with open(adapterRoot + '/from_' + id + '.proto', 'wb') as file:
+                        resp = requests.get(protoFromLink)
+                        file.write(resp.content)
 
-                p = sub.call('protoc -I=' + adapterRoot + ' --python_out=' + adapterRoot + ' ' + adapterRoot + '/from_' + id + '.proto', shell=True)
-                itemEntry["protoFrom"] = catJSON["items"][0]["serialization_from_device"]["schema_ref"]["mainMessageName"]
-                flag = flag + 1
-            except:
-                print("Couldn't get *From* Proto")
+                    p = sub.call('protoc -I=' + adapterRoot + ' --python_out=' +
+                                 adapterRoot + ' ' + adapterRoot + '/from_' + id + '.proto', shell=True)
+                    itemEntry["protoFrom"] = catJSON["items"][0]["serialization_from_device"]["schema_ref"]["mainMessageName"]
+                    flag = flag + 1
+                except:
+                    print("Couldn't get *From* Proto")
 
-            try:  # ADD SERVER NAME ENTRY HERE
-                # serverName = catJSON["items"][0]["server_name"][]
+                serverName = catJSON["items"][0]["server_config"]["server_name"]
 
-            items[id] = itemEntry
-            itemEntry["id"] = id
-            print(itemEntry)
-            mcln.insert_one(items)
-            if(flag == 2):
+                serverConfig = json.loads(servers[serverName])
+                try:
+                    itemEntry["serverName"] = serverConfig["server_name"]
+                    itemEntry["getAdd"] = serverConfig["get_address"].replace(
+                        "{id}", id)
+                    itemEntry["postAdd"] = serverConfig["post_address"].replace(
+                        "{id}", id)
+                    itemEntry["getReqBody"] = serverConfig["get_request_body"].replace(
+                        "{id}", id)
+                    itemEntry["getRespBody"] = serverConfig["get_response_body"].replace(
+                        "{id}", id)
+                    itemEntry["postReqBody"] = serverConfig["post_request_body"].replace(
+                        "{id}", id)
+                    itemEntry["postRespBody"] = serverConfig["post_response_body"].replace(
+                        "{id}", id)
+                    flag = flag + 1
+                except:
+                    print("Couldn't add server API configuration")
+
+                items[id] = itemEntry
+                itemEntry["id"] = id
+                print(itemEntry)
+                mcln.insert_one(json.dumps({id: itemEntry}))
+
+            elif(catJSON["items"][0]["server_config"]["protocol"] == ("http" or "HTTP" or "https" or "HTTPS")):
+
+                serverName = catJSON["items"][0]["server_config"]["server_name"]
+
+                serverConfig = json.loads(servers[serverName])
+                try:
+                    itemEntry["serverName"] = serverConfig["server_name"]
+                    itemEntry["getAdd"] = serverConfig["get_address"].replace(
+                        "{id}", id)
+                    itemEntry["postAdd"] = serverConfig["post_address"].replace(
+                        "{id}", id)
+                    itemEntry["getReqBody"] = serverConfig["get_request_body"].replace(
+                        "{id}", id)
+                    itemEntry["getRespBody"] = serverConfig["get_response_body"].replace(
+                        "{id}", id)
+                    itemEntry["postReqBody"] = serverConfig["post_request_body"].replace(
+                        "{id}", id)
+                    itemEntry["postRespBody"] = serverConfig["post_response_body"].replace(
+                        "{id}", id)
+                    flag = 3
+                except:
+                    print("Couldn't add server API configuration")
+
+                items[id] = itemEntry
+                itemEntry["id"] = id
+                print(itemEntry)
+                hcln.insert_one(json.dumps({id: itemEntry}))
+
+            if(flag == 3):
                 flag = 0
                 socket.send_string(json.dumps(itemEntry))
 
@@ -98,7 +167,18 @@ class Register(Resource):
             print(e)
 
 
-api.add_resource(Register, '/register')
+api.add_resource(DeviceRegister, '/device_register')
+
+
+class ServerRegister(Resource):
+    def post(self):
+        catJSON = request.get_json()
+        server_name = catJSON["server_name"]
+        servers.update({server_name: catJSON})
+        scln.insert_one(json.dumps{server_name: catJSON})
+
+
+api.add_resource(ServerRegister, '/server_register')
 
 
 def main():
